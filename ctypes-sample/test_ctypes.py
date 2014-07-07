f4f110cb2aab7195795bea96b75b9b5b805ee3b0
gf0842wf <gf0842wf@gmail.com>
2014-07-07 18:32:30 +0800

    ctypes string
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- ctypes-sample/test_ctypes.py
+++ ctypes-sample/test_ctypes.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from ctypes import *
 import time
 
 libc = CDLL("./acc.so")
-libc.acc.restype = c_ulonglong
+libc.acc.restype = c_ulonglong # 需要明确指明返回类型
+# libc.say_hello.restype = c_char_p
 t0 = time.time()
 print libc.acc()
 print time.time() - t0
